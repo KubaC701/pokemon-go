f634657d27971527f9a2ea423979b663b1bb004a
Jakub Ciepły <jakub.cieply@displate.com>
2024-01-21 14:55:12 +0100

    Add navigation bar

--- app/src/main/java/com/cdv/pokemongo/App.kt
+++ app/src/main/java/com/cdv/pokemongo/App.kt
@@ -1,10 +1,14 @@
 package com.cdv.pokemongo
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.cdv.pokemongo.ui.composables.BottomNavigationBar
 import com.cdv.pokemongo.ui.screens.Bag
 import com.cdv.pokemongo.ui.screens.CatchPokemon
 import com.cdv.pokemongo.ui.screens.Encyclopedia
@@ -23,26 +27,30 @@ enum class Screen() {
 
 @Composable
 fun App(navController: NavHostController = rememberNavController()) {
-
-    NavHost(navController = navController, startDestination = Screen.Main.name) {
-        composable(Screen.Main.name) {
-            Main(navController)
-        }
-        composable(Screen.Bag.name) {
-            Bag(navController)
-        }
-        composable(Screen.Profile.name) {
-            Profile(navController)
-        }
-        composable(Screen.Settings.name) {
-            Settings(navController)
-        }
-        composable(Screen.Encyclopedia.name) {
-            Encyclopedia(navController)
-        }
-        composable(Screen.CatchPokemon.name) {
-            CatchPokemon(navController)
+    Scaffold(bottomBar = { BottomNavigationBar(navController = navController) }) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Main.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Main.name) {
+                Main(navController)
+            }
+            composable(Screen.Bag.name) {
+                Bag(navController)
+            }
+            composable(Screen.Profile.name) {
+                Profile(navController)
+            }
+            composable(Screen.Settings.name) {
+                Settings(navController)
+            }
+            composable(Screen.Encyclopedia.name) {
+                Encyclopedia(navController)
+            }
+            composable(Screen.CatchPokemon.name) {
+                CatchPokemon(navController)
+            }
         }
     }
-
 }
---
+++ app/src/main/java/com/cdv/pokemongo/ui/composables/NavigationBar.kt
@@ -0,0 +1,69 @@
+package com.cdv.pokemongo.ui.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.cdv.pokemongo.Screen
+import com.cdv.pokemongo.ui.theme.Pink80
+import com.cdv.pokemongo.ui.theme.Purple40
+import com.cdv.pokemongo.ui.theme.Purple80
+
+
+data class BottomNavItem(val route: String, val icon: ImageVector, val label: String)
+
+val BottomNavItems = listOf(
+    BottomNavItem(Screen.Profile.name, Icons.Default.Person, "Profile"),
+    BottomNavItem(Screen.Main.name, Icons.Default.Home, "Game"),
+    BottomNavItem(Screen.Bag.name, Icons.Default.Menu, "Backpack")
+)
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .background(Purple80)
+            .padding(8.dp, 16.dp), Arrangement.SpaceBetween
+
+    ) {
+
+        BottomNavItems.forEach { item ->
+            IconButton(
+                modifier = Modifier.background(if (currentRoute == item.route) Pink80 else Purple40),
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                    }
+                },
+            ) {
+                Icon(item.icon, contentDescription = item.label)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NavigationBarPreview() {
+    BottomNavigationBar(rememberNavController())
+}
