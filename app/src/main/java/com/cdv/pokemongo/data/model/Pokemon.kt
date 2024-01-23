83f5fd1b83fe09698a128570a68cb21f2b46b773
Jakub Ciepły <jakub.cieply@displate.com>
2024-01-23 22:03:32 +0100

    Add level to pokemons model

--- app/src/main/java/com/cdv/pokemongo/data/model/Pokemon.kt
+++ app/src/main/java/com/cdv/pokemongo/data/model/Pokemon.kt
@@ -3,15 +3,17 @@ package com.cdv.pokemongo.data.model
 import android.util.Log
 import org.json.JSONObject
 
-class Pokemon(pokemonJson : JSONObject) {
+class Pokemon(pokemonJson: JSONObject) {
     val id: Int = pokemonJson.getInt("id")
     val name: String = pokemonJson.getString("name")
     val sprites: Sprites = Sprites(
         pokemonJson.getJSONObject("sprites").getString("front_default"),
-        pokemonJson.getJSONObject("sprites").getJSONObject("other").getJSONObject("official-artwork").getString("front_default")
+        pokemonJson.getJSONObject("sprites").getJSONObject("other")
+            .getJSONObject("official-artwork").getString("front_default")
     )
+    val level: Int = (1..10).random()
     fun showPokemonData() {
-    Log.d("POKEMON OBJECT DATA", this.id.toString());
+        Log.d("POKEMON OBJECT DATA", this.id.toString());
         Log.d("POKEMON OBJECT DATA", this.name);
         Log.d("POKEMON OBJECT DATA", this.sprites.lowRes);
         Log.d("POKEMON OBJECT DATA", this.sprites.highRes);
