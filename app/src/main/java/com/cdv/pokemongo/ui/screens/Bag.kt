d6cafe653623e2fc08eb52757d5598d02e462d47
Sury1920 <filip.szpulecki@gmail.com>
2024-01-27 18:48:10 +0100

    Switched to Title component

--- app/src/main/java/com/cdv/pokemongo/ui/screens/Bag.kt
+++ app/src/main/java/com/cdv/pokemongo/ui/screens/Bag.kt
@@ -6,28 +6,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.cdv.pokemongo.ui.composables.MyPokemon
+import com.cdv.pokemongo.ui.composables.Title
 import com.cdv.pokemongo.ui.models.BackpackModel
 import com.cdv.pokemongo.ui.theme.BackgroundColor
-import com.cdv.pokemongo.ui.theme.TertiaryColor
 
 @Composable
 fun Bag(backpackModel: BackpackModel = viewModel()) {
@@ -41,11 +35,7 @@ fun Bag(backpackModel: BackpackModel = viewModel()) {
         ) {
             Spacer(Modifier.padding(4.dp))
 
-            Row(Modifier.height(100.dp).clip(RoundedCornerShape(20.dp)).fillMaxWidth()){
-                Box(Modifier.fillMaxSize().background(TertiaryColor), contentAlignment = Alignment.Center){
-                    Text(text = "Moje Potwory", fontSize = 36.sp, fontWeight = FontWeight.Bold)
-                }
-            }
+            Title("Moje Potwory")
 
             Spacer(Modifier.padding(4.dp))
             uiState.pokemons.forEach { pokemon ->
